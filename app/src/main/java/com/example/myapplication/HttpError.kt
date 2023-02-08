0e98a192d4d5d7036dab22f4ba350e6df0ffee82
amberhasan <amber.hasan@gmail.com>
2023-02-08 12:13:23 -0600

    Sealed interface (not as ..popular..)

--- app/src/main/java/com/example/myapplication/HttpError.kt
+++ app/src/main/java/com/example/myapplication/HttpError.kt
@@ -1,23 +1,12 @@
 package com.example.myapplication
 
-// Instances. More customization!!!!
-//Sealed classes are just like enum classes but more customization
-//Always ask yourself if we need individual behavior, for example the "reason" vs not.
-sealed class HttpError(val code: Int){
-    data class Unauthorized(val reason: String): HttpError(401) //like, Unauthorized can have different instances w diff reasons.
-    object NotFound: HttpError(404)
 
-    fun doSomething() {
-
-    }
+sealed interface HttpError{
+    data class Unauthorized(val reason: String): HttpError
+    object NotFound: HttpError
 }
 
-//Constants, cannot have individual instances in enum - can do codes.
 enum class HttpErrorEnum(val code: Int) {
     Unauthorized(401),
     NotFound(404);
-
-    fun doSomething() {
-
-    }
 }

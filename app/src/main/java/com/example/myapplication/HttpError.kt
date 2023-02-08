6769637be707b19172f8f253a65af9a7ad4ebc5c
amberhasan <amber.hasan@gmail.com>
2023-02-08 12:08:58 -0600

    Sealed classes are more for INSTANCES and enum classes are more for CONSTANTS. Sealed classes can use data classes to offer more customized instances

--- app/src/main/java/com/example/myapplication/HttpError.kt
+++ app/src/main/java/com/example/myapplication/HttpError.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
-//No other modules can extend this class
+// Instances. More customization!!!!
+//Sealed classes are just like enum classes but more customization
+//Always ask yourself if we need individual behavior, for example the "reason" vs not.
 sealed class HttpError(val code: Int){
-    object Unauthorized: HttpError(401)
+    data class Unauthorized(val reason: String): HttpError(401) //like, Unauthorized can have different instances w diff reasons.
     object NotFound: HttpError(404)
 
     fun doSomething() {
@@ -10,6 +12,7 @@ sealed class HttpError(val code: Int){
     }
 }
 
+//Constants, cannot have individual instances in enum - can do codes.
 enum class HttpErrorEnum(val code: Int) {
     Unauthorized(401),
     NotFound(404);

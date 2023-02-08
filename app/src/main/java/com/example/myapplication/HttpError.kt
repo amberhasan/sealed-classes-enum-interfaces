8e51a9538ed3aaf96557ee15ca790494b37cc47b
amberhasan <amber.hasan@gmail.com>
2023-02-08 11:55:33 -0600

    Sealed classes with parameter

--- app/src/main/java/com/example/myapplication/HttpError.kt
+++ app/src/main/java/com/example/myapplication/HttpError.kt
@@ -1,9 +1,7 @@
 package com.example.myapplication
 
 //No other modules can extend this class
-sealed class HttpError {
-
-    //We can have subclasses that describe the HTTP Error.
-    object Unauthorized: HttpError()
-    object NotFound: HttpError()
+sealed class HttpError(val code: Int){
+    object Unauthorized: HttpError(401)
+    object NotFound: HttpError(404)
 }
